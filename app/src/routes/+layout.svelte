dd204dd23c187d7a6cd1796a98b8aefc6f6724a0
esteinig <eike.steinig@unimelb.edu.au>
2023-12-27 07:17:41 +1100

    chore(app): initiate application code

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { Modal, storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+
+</script>
+
+
+<Modal />
+<slot />
